cde024817c2acb28cba28cfacf06683172cca588
ryotaro tsuji <tsuji.xs@au.com>
2020-02-20 10:46:44 +0900

    Initial Commit

---
+++ FlappyBird2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlappyBird2
+//
+//  Created by Apple on 2020/02/20.
+//  Copyright © 2020 ryotaro.tsuji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
